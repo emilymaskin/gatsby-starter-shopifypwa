3e8bd408e553d2b03dc917c2ec0eb671668aa285
Emily Maskin <emily.maskin@gmail.com>
2020-04-13 14:06:46 -0400

    top 5 on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 
-const IndexPage = () => (
-  <>
-    <Link to="/products/">All Products</Link>
-  </>
-);
+const IndexPage = ({ data }) => {
+  const { products } = data.shopify.shop.collectionByHandle;
+
+  return (
+    <>
+      <div>
+        <Link to="/products/">All Products</Link>
+      </div>
+      <div>
+        <Link to="/collections/">All Collections</Link>
+      </div>
+
+      <div>
+        {products.edges.map((product, index) => {
+          const { title, handle, images } = product.node;
+          const { originalSrc, altText } = images.edges[0].node;
+
+          return (
+            <div key={index}>
+              <a href={`/products/${handle}`}>
+                <img src={originalSrc} alt={altText} />
+                <div>{title}</div>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default IndexPage;
+
+export const query = graphql`
+  query homepageQuery {
+    shopify {
+      shop {
+        collectionByHandle(handle: "best-sellers") {
+          products(first: 5) {
+            edges {
+              node {
+                title
+                handle
+                images(first: 1, maxWidth: 200) {
+                  edges {
+                    node {
+                      originalSrc
+                      altText
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
