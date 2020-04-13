021192f48177fa1f4577675e90593865ecfec91b
Emily Maskin <emily.maskin@gmail.com>
2020-04-13 13:32:20 -0400

    collections page

---
+++ src/pages/collections.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+class Collections extends React.Component {
+  render() {
+    const collections = this.props.data.shopify.shop.collections;
+
+    return (
+      <div>
+        <h1>All Collections</h1>
+        {collections.edges.map((edge, index) => (
+          <div key={index}>
+            <a href={`/collections/${edge.node.handle}`}>{edge.node.title}</a>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Collections;
+
+export const query = graphql`
+  query collectionsQuery {
+    shopify {
+      shop {
+        collections(first: 250) {
+          edges {
+            node {
+              id
+              handle
+              title
+              image {
+                originalSrc
+                altText
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+          }
+        }
+      }
+    }
+  }
+`;
